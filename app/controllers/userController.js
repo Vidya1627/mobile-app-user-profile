246026eb9e94b9e98acad05ed475c242107224bb
pravi <pravi@pravis-MacBook-Air.local>
2023-05-08 17:32:30 +0530

    fix login signup

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,28 +3,23 @@ const otpService = require('../services/otpService');
 
 exports.login = async (req, res) => {
   try {
-    const { email, otp } = req.body;
+    const { email } = req.body;
 
     // Find the user by email
     let user = await User.findOne({ email });
 
-    // If the user doesn't exist, create a new one
+    // If the user doesn't exist, show the msg to sign up
     if (!user) {
-      // Generate a random 6-digit OTP
-      const generatedOTP = otpService.generateOTP();
-
-      // Save the user with the email and OTP
-      user = new User({ email, otp: generatedOTP });
-      await user.save();
-
-      console.log(`OTP for email ${email}: ${generatedOTP}`);
+        return res.status(401).json({ message: 'User not registered. Please sign up' });
     }
 
-    // Verify the entered OTP
-    if (user.otp !== otp) {
-      return res.status(401).json({ message: 'Invalid OTP' });
-    }
+    //    // Verify the entered OTP
+    //   if (user.otp !== otp) {
+    //     return res.status(401).json({ message: 'Invalid OTP' });
+    //   }
 
+    const generatedOTP = otpService.generateOTP();
+    console.log(`OTP for email ${email}: ${generatedOTP}`);
     // Successful login
     return res.status(200).json({ message: 'Login successful' });
   } catch (error) {
@@ -32,3 +27,34 @@ exports.login = async (req, res) => {
     return res.status(500).json({ message: 'Internal server error' });
   }
 };
+
+exports.signup = async (req, res) => {
+    try {
+      const { email } = req.body;
+  
+      // Find the user by email
+      let user = await User.findOne({ email });
+
+      // If the user exists, show the msg to log in
+      if(user) {
+        return res.status(401).json({ message: 'User already registered. Please log in' });
+      }
+      // If the user doesn't exist, create a new one
+      if (!user) {
+        // Generate a random 6-digit OTP
+        const generatedOTP = otpService.generateOTP();
+  
+        // Save the user with the email and OTP
+        user = new User({ email, otp: generatedOTP });
+        await user.save();
+  
+        console.log(`OTP for email ${email}: ${generatedOTP}`);
+      }
+      
+      // Successful login
+      return res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  };
--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/login', userController.login);
+router.post('/signup', userController.signup);
 
 module.exports = router;
